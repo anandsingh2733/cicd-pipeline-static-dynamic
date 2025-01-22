2da24a6e15994adee5195f996cb4a8ec15f0c581
anandsingh2733 <92663136+anandsingh2733@users.noreply.github.com>
2025-01-22 12:23:24 +0900

    Update lambda_function.py

--- Dynamic/lambda_function.py
+++ Dynamic/lambda_function.py
@@ -1,8 +1,8 @@
-nimport json
+import json
 
 def lambda_handler(event, context):
     # This Lambda function just returns a message
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda Verion2')
+        'body': json.dumps('Hello from Lambda Verion2 !!!')
     }
